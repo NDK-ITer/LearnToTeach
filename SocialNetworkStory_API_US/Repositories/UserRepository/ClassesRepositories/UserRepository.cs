d6fa3f6fe495077e9c15d717f465d6b643610196
NDK-ITer <116534879+NDK-ITer@users.noreply.github.com>
2023-06-15 15:24:10 +0700

    user service api initial

---
+++ SocialNetworkStory_API_US/Repositories/UserRepository/ClassesRepositories/UserRepository.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+//using Microsoft.IdentityModel.JsonWebTokens;
+using MyDemoAPIAsp.NETCore.Data;
+using MyDemoAPIAsp.NETCore.Models;
+using MyDemoAPIAsp.NETCore.Repositories.UserRepository.InterfaceRepositories;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace MyDemoAPIAsp.NETCore.Repositories.UserRepository.ClassesRepositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly IConfiguration configuration;
+
+        public UserRepository(UserManager<ApplicationUser> userManager, 
+                              SignInManager<ApplicationUser> signInManager, 
+                              IConfiguration configuration) 
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.configuration = configuration;
+        }
+        public async Task<string> Login(LoginModel loginModel)
+        {
+            var result = await signInManager.PasswordSignInAsync(loginModel.Email, loginModel.Password, false, false);
+            if (!result.Succeeded)
+            {
+                return string.Empty;
+            }
+            var authClaims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Email, loginModel.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                
+            };
+            var authenKey = new SymmetricSecurityKey (Encoding.UTF8.GetBytes(configuration["JWT:Secret"]));
+            var token = new JwtSecurityToken(
+                issuer: configuration["JWT:ValidIssuer"],
+                audience: configuration["JWT:ValidAudience"],
+                expires: DateTime.Now.AddMinutes(60),
+                claims: authClaims,
+                signingCredentials: new SigningCredentials(authenKey, SecurityAlgorithms.HmacSha512Signature)
+                );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public async Task<IdentityResult> Register(RegisterModel registerModel)
+        {
+            var user = new ApplicationUser
+            {
+                FirstName = registerModel.FirstName,
+                LastName = registerModel.LastName,
+                Email = registerModel.Email,
+                UserName = registerModel.Email,
+                CreateDate = DateTime.Now,
+                Avatar = registerModel.Avatar,
+            };
+
+            return await userManager.CreateAsync(user, registerModel.Password);
+        }
+    }
+}
---
+++ SocialNetworkStory_API_US/Repositories/UserRepository/InterfacesRepositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using MyDemoAPIAsp.NETCore.Models;
+
+namespace MyDemoAPIAsp.NETCore.Repositories.UserRepository.InterfaceRepositories
+{
+    public interface IUserRepository
+    {
+        public Task<IdentityResult> Register(RegisterModel registerModel);
+        public Task<string> Login(LoginModel loginModel);
+        //public Task<IdentityResult> Logout();
+    }
+}
