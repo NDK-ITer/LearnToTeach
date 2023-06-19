ab870cd56414c2da65cf4a68166435b49997e71c
NDK-ITer <116534879+NDK-ITer@users.noreply.github.com>
2023-06-19 15:57:09 +0700

    update to classroom microservice

---
+++ ClassroomMicroservice_API_AS/Repositories/AccountRepository/InterfacesRepositories/IAccountRepository.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using MyDemoAPIAsp.NETCore.Models;
+
+namespace MyDemoAPIAsp.NETCore.Repositories.AccountRepository.InterfaceRepositories
+{
+    public interface IAccountRepository
+    {
+        public Task<IdentityResult> Register(RegisterModel registerModel);
+        public Task<string> Login(LoginModel loginModel);
+        //public Task<IdentityResult> Logout();
+    }
+}
--- SocialNetworkStory/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SocialNetworkStory.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
