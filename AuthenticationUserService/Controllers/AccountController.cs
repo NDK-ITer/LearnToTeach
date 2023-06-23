27d395a22631431f17eb60e0896052cbe7f29f07
NDK-ITer <116534879+NDK-ITer@users.noreply.github.com>
2023-06-23 15:42:26 +0700

    authentication jwt 01

---
+++ AuthenticationUserService/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using JwtAuthenticationManager;
+using JwtAuthenticationManager.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationUserService.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly JwtTokenHandler _jwtTokenHandler;
+
+        public AccountController(JwtTokenHandler jwtTokenHandler)
+        {
+            _jwtTokenHandler = jwtTokenHandler;
+        }
+
+        public ActionResult<AuthenticationResponse> Authentication([FromBody] AuthenticationRequest authenticationRequest)
+        {
+            var authenticationResponse = _jwtTokenHandler.GenerateJwtToken(authenticationRequest);
+            if (authenticationResponse != null) { return Unauthorized(); }
+            return authenticationResponse;
+        }
+    }
+}
