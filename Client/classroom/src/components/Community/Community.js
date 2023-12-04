2168e06050afb6175439fa2ab50dfb1f9aaae854
ely.nguyen <ely.nguyen@tpfgroup.cc>
2023-12-05 01:52:01 +0700

    grades

--- Client/classroom/src/components/Community/Community.js
+++ Client/classroom/src/components/Community/Community.js
@@ -8,24 +8,58 @@ import CancelOutlinedIcon from '@material-ui/icons/CancelOutlined';
 import classApi from 'api/classApi';
 import Role from 'constants/role';
 import "./style.css";
+import { useLocalContext } from 'context';
+import { ConfirmationNumber } from '@material-ui/icons';
+import { useSnackbar } from 'notistack';
+import ConfirmationDialog from 'components/ConfirmationDialog';
 
 
 const Community = ({ classData }) => {
 
+  const { user } = useLocalContext();
   const [userHost, settuserHost] = useState([]);
+  const [isUserHost, settIsUserHost] = useState(false);
   const [userMember, setuserMember] = useState([]);
   const [countuser, setcountuser] = useState(0);
+  const { enqueueSnackbar } = useSnackbar();
   useEffect(() => {
     const fetchData = async () => {
       const params = new URLSearchParams([['idClassroom', classData.idClassroom]]);
       const result = await classApi.getClassById(params);
       settuserHost(result.listMembers.filter(x => x.role == Role.HOST));
+      settIsUserHost(result.listMembers.filter(x => x.role == Role.HOST && x.idMember == user.id).length > 0 ? true : false);
       setuserMember(result.listMembers.filter(x => x.role == Role.MEMBER));
       setcountuser(result.listMembers.filter(x => x.role == Role.MEMBER).length)
     };
     fetchData();
   }, []);
+  const [dialogOpenMember, setDialogOpenMember] = useState(false);
+  const [idMember, setIdMember] = useState(null);
+  const handleIdMenber = (item) => {
+    setIdMember(item);
+    setDialogOpenMember(true);
+  };
+  const handleCloseMember = () => {
+    setIdMember(null);
+    setDialogOpenMember(false);
+  };
+  const handledeleteMember = async () => {
+    try {
+      const params = new URLSearchParams([['idClassroom', classData.idClassroom], ['idMember', idMember]]);
+      const result = await classApi.removemember(params);
+      console.log(result)
+      if (result.status == 1) {
+        enqueueSnackbar(result.message, { variant: 'success' });
+        window.location.reload();
+      } else {
+        enqueueSnackbar(result.message, { variant: 'error' });
+      }
 
+    } catch (error) {
+      console.log('Failed to login:', error);
+      enqueueSnackbar(error.message, { variant: 'error' });
+    }
+  };
   return (
     <div>
       <div className='role'>
@@ -39,7 +73,7 @@ const Community = ({ classData }) => {
                 <PermContactCalendarOutlinedIcon />
               </Avatar>
               <div className='name'>{item.nameMember}</div>
-            </div>           
+            </div>
           </li>
         ))}
 
@@ -52,26 +86,25 @@ const Community = ({ classData }) => {
         {userMember.map((item, index) => (
           <li key={index} className='information'>
             <div className='student_information'>
-              <Avatar style={{ m: 1, backgroundColor: 'deeppink'}}>
+              <Avatar style={{ m: 1, backgroundColor: 'deeppink' }}>
                 <PermIdentityOutlinedIcon />
               </Avatar>
               <div className='name'>{item.nameMember}</div>
-            </div>           
+            </div>
             <div>
-              <Button startIcon={<LockOpenOutlinedIcon/>} style={{marginRight: '10px'}}>
-                Mở khóa
-              </Button>
-              <Button startIcon={<LockOutlinedIcon/>} style={{marginRight: '10px'}}>
-                Khóa
-              </Button>
-              <Button startIcon={<CancelOutlinedIcon/>} style={{marginRight: '10px'}}>
+              {isUserHost && <Button onClick={() => handleIdMenber(item.idMember)} startIcon={<CancelOutlinedIcon />} style={{ marginRight: '10px' }}>
                 Xóa
-              </Button>
+              </Button>}
             </div>
           </li>
         ))}
-
       </ul>
+      <ConfirmationDialog
+        open={dialogOpenMember}
+        onClose={handleCloseMember}
+        onConfirm={handledeleteMember}
+        message="Are you sure you want to Notify?"
+      />
     </div>
   )
 }
