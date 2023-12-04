b371de0655f367709c5185b3ffbd754a83e5604a
ely.nguyen <ely.nguyen@tpfgroup.cc>
2023-12-04 23:01:27 +0700

    grades

--- Client/classroom/src/components/Grade/GradeDetail/GradeDetail.js
+++ Client/classroom/src/components/Grade/GradeDetail/GradeDetail.js
@@ -7,6 +7,7 @@ import classApi from 'api/classApi';
 import Role from 'constants/role';
 import { Redirect, Route, Switch, useRouteMatch } from 'react-router-dom';
 import SubmitExercise from 'components/Exercises/SubmitExercise/SubmitExercise';
+import GoBackButton from 'components/GoBackButton';
 
 const GradeDetail = ({ exercise, classData, userHost }) => {
     const match = useRouteMatch();
@@ -14,53 +15,80 @@ const GradeDetail = ({ exercise, classData, userHost }) => {
     const userNotGraded = exercise.listAnswer.filter(x => x.point == null);
     // const countUser = classData.listMembers.filter(x=>x.role==Role.MEMBER).length;
     const listUserAnswer = exercise.listAnswer;
-
+    const isexerciseGraded = userGraded.length == listUserAnswer.length ? true : false;
     return (
         <div>
-            <div className='role'>
-                <h1>Sinh viên đã chấm</h1>
-                <div className='quantity'>({userGraded.length}) sinh viên</div>
-            </div>
-            <ul className='list_informations'>
-                {userGraded.map((item, index) => (
-                    <li key={index} className='graded_information'>
-                        <a href={`${match.url}/answer/${item.idMember}`}>
-                            <Avatar style={{ m: 1, backgroundColor: 'rgb(4, 214, 46)'}}>
-                                <PermContactCalendarOutlinedIcon />
-                            </Avatar>
-                        </a>
-                        <div className='submit_information'>
-                            <div className='name'>{classData.listMembers.find(x => x.idMember == item.idMember).nameMember}</div>
-                            <div className='grade'>Điểm số: </div>
-                        </div>
-                        
-                    </li>
-                ))}
+            {isexerciseGraded && <div>
+                <div className='role'>
+                    <h1>bảng điểm</h1>
+                    <div className='quantity'>({userGraded.length}) sinh viên</div>
+                </div>
+                <ul className='list_informations'>
+                    {userGraded.map((item, index) => (
+                        <li key={index} className='graded_information'>
+                            <a href={`${match.url}/answer/${item.idMember}`}>
+                                <Avatar style={{ m: 1, backgroundColor: 'rgb(4, 214, 46)' }}>
+                                    <PermContactCalendarOutlinedIcon />
+                                </Avatar>
+                            </a>
+                            <div className='submit_information'>
+                                <div className='name'>{classData.listMembers.find(x => x.idMember == item.idMember).nameMember}</div>
+                                <div className='grade'>Điểm số:{item.point} </div>
+                            </div>
 
-            </ul>
-            <div className='role'>
-                <h1>Sinh viên chưa chấm</h1>
-                <div className='quantity'>({userNotGraded.length}) sinh viên</div>
-            </div>
-            <ul className='list_informations'>
-                {userNotGraded.map((item, index) => (
-                    <li key={index} className='submit_student_information'>
-                        <a href={`${match.url}/answer/${item.idMember}`}>
-                            <Avatar style={{ m: 1, backgroundColor: 'rgb(227, 227, 0)'}}>
-                                <PermIdentityOutlinedIcon />
-                            </Avatar>
-                        </a>
-                        <div className='submit_information'>
-                            <div className='name'>{classData.listMembers.find(x => x.idMember == item.idMember).nameMember}</div>
-                            <div>Thời gian nộp</div>
-                        </div>
+                        </li>
+                    ))}
 
-                    </li>
-                ))}
+                </ul>
+                <GoBackButton />
+            </div>}
+            {!isexerciseGraded && <div>
+                <div className='role'>
+                    <h1>Sinh viên đã chấm</h1>
+                    <div className='quantity'>({userGraded.length}) sinh viên</div>
+                </div>
+                <ul className='list_informations'>
+                    {userGraded.map((item, index) => (
+                        <li key={index} className='graded_information'>
+                            <a href={`${match.url}/answer/${item.idMember}`}>
+                                <Avatar style={{ m: 1, backgroundColor: 'rgb(4, 214, 46)' }}>
+                                    <PermContactCalendarOutlinedIcon />
+                                </Avatar>
+                            </a>
+                            <div className='submit_information'>
+                                <div className='name'>{classData.listMembers.find(x => x.idMember == item.idMember).nameMember}</div>
+                                <div className='grade'>Điểm số:{item.point} </div>
+                            </div>
 
-            </ul>
+                        </li>
+                    ))}
 
+                </ul>
+                <div className='role'>
+                    <h1>Sinh viên chưa chấm</h1>
+                    <div className='quantity'>({userNotGraded.length}) sinh viên</div>
+                </div>
+                <ul className='list_informations'>
+                    {userNotGraded.map((item, index) => (
+                        <li key={index} className='submit_student_information'>
+                            <a href={`${match.url}/answer/${item.idMember}`}>
+                                <Avatar style={{ m: 1, backgroundColor: 'rgb(227, 227, 0)' }}>
+                                    <PermIdentityOutlinedIcon />
+                                </Avatar>
+                            </a>
+                            <div className='submit_information'>
+                                <div className='name'>{classData.listMembers.find(x => x.idMember == item.idMember).nameMember}</div>
+                                <div>Thời gian nộp</div>
+                            </div>
+
+                        </li>
+                    ))}
+
+                </ul>
+                <GoBackButton />
+            </div>}
         </div>
+
     )
 }
 
