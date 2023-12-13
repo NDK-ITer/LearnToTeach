43dc3061c2abdf8a11c4194da156cc17598c7322
NDK-ITer <116534879+NDK-ITer@users.noreply.github.com>
2023-12-13 19:46:51 +0700

    update send mail

--- Server/ClassService/ClassServer/Consumers/MemberConsumers/MemberLeaveIsValidConsumer/GenerateMemberLeaveIsValidConsumer.cs
+++ Server/ClassService/ClassServer/Consumers/MemberConsumers/MemberLeaveIsValidConsumer/GenerateMemberLeaveIsValidConsumer.cs
@@ -1,4 +1,6 @@
 ﻿using Application.Services;
+using Domain.Entities;
+using Events.ClassroomServiceEvents;
 using Events.ClassroomServiceEvents.Member.MemberLeaveClassroom;
 using MassTransit;
 
@@ -15,8 +17,19 @@ namespace ClassServer.Consumers.MemberConsumers.MemberLeaveIsValidConsumer
         public async Task Consume(ConsumeContext<ILeaveClassroomIsValidEvent> context)
         {
             var data = context.Message;
+            var classroom = unitOfWork_ClassroomService._classroomService.GetClassroomById(data.IdClassroom);
+            var member = unitOfWork_ClassroomService._memberService.GetMemberById(data.IdUser);
             var check = unitOfWork_ClassroomService._classroomService.RemoveMember(data.IdClassroom, data.IdUser);
-            Console.WriteLine(check);
+            if (check == 1)
+            {
+                await context.Publish<IClassroomSendEmail>(new
+                {
+                    IdMessage = Guid.NewGuid(),
+                    Email = member.Email,
+                    Subject = $"classroom {classroom.Name} have been delete.",
+                    Content = $"Dear {member.Name}</br>You have been kicked or get out classroom {classroom.Name}."
+                });
+            }
         }
     }
 }
