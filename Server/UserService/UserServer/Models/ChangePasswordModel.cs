c4b697cc3834b88ab9b4e0bbbb5af85f5020367d
NDK-ITer <116534879+NDK-ITer@users.noreply.github.com>
2023-12-15 10:40:11 +0700

    Create ChangePasswordModel.cs

---
+++ Server/UserService/UserServer/Models/ChangePasswordModel.cs
@@ -0,0 +1,10 @@
+﻿namespace UserServer.Models
+{
+    public class ChangePasswordModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string NewPassword { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
